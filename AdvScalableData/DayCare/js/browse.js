a5633eca74c68fed640ed55c9f34d6e6372f7c37
JDNorrod <Kingjadien@aol.com>
2012-05-07 15:18:10 -0700

    browse.js and refresh.js added

---
+++ AdvScalableData/DayCare/js/browse.js
@@ -0,0 +1,19 @@
+/*
+"entry 3":{
+    "group": ["Age Group: ", "Infant"],
+        "fname": ["First Name:", "Sashi"],
+        "lname": ["Last Name:", "Norrod"],
+        "bday": ["Birthday: ", "2007-08-21"],
+        "slider": ["Age: ", "1"],
+        "allergy": ["Has Allergy?: ", "No"],
+        "trained": ["Is Trained?:", "No"],
+        "comment": ["Message: ", "One of the coolest kids ever!"]
+},
+*/
+
+$('#infants').live('pageinit', function(){
+
+
+
+
+});
