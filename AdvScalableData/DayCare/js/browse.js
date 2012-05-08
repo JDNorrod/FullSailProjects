14331c0538d92b5f52398cdc843701fad5afb392
JDNorrod <Kingjadien@aol.com>
2012-05-07 19:34:07 -0700

    json list working, not like I want it to though

--- AdvScalableData/DayCare/js/browse.js
+++ AdvScalableData/DayCare/js/browse.js
@@ -1,35 +1,41 @@
 /*
-"entry 3":{
-    "group": ["Age Group: ", "Infant"],
-        "fname": ["First Name:", "Sashi"],
-        "lname": ["Last Name:", "Norrod"],
-        "bday": ["Birthday: ", "2007-08-21"],
-        "slider": ["Age: ", "1"],
-        "allergy": ["Has Allergy?: ", "No"],
-        "trained": ["Is Trained?:", "No"],
-        "comment": ["Message: ", "One of the coolest kids ever!"]
+'entry 3':{
+    'group': ['Age Group: ', 'Infant'],
+        'fname': ['First Name:', 'Sashi'],
+        'lname': ['Last Name:', 'Norrod'],
+        'bday': ['Birthday: ', '2007-08-21'],
+        'slider': ['Age: ', '1'],
+        'allergy': ['Has Allergy?: ', 'No'],
+        'trained': ['Is Trained?:', 'No'],
+        'comment': ['Message: ', 'One of the coolest kids ever!']
 },
 */
 
 $('#infants').live('pageinit', function(){
 
     $.ajax({
-        url: 'xhr/infants.json',
-        type: "GET",
-        dataType: "json",
+        url: 'xhr/data.json',
+        type: 'GET',
+        dataType: 'json',
         success: function(resp){
             console.log(resp);
-        }
-
-    })
-
-   /* for(var j in json){
-        console.log(json);
-        $('<li><h4>' +
-            lname[1]  + ', ' +
-            fname[1] + '&nbsp;&nbsp;Edit &nbsp;Delete</h4>' +
-            '<p>Age: ' + slider[1] + '&nbsp;&nbsp; Birthday: ' +
-            bday[1] + '&nbsp;&nbsp;LifeGroup: ' + trained[1] + '</p></li>').appendTo('#underSix');
-    }*/
+            for(var i = 0, len=resp.request.length; i < len; i++){
+                var item = resp.request[i];
+                console.log('Item is: ', item);
+                 $('#underSix').append($(' ' + '<li><h4>' +
+                    item.lname[1]  +
+                    ', ' +
+                    item.fname[1] +
+                    '&nbsp;&nbsp;Edit &nbsp;Delete</h4>' +
+                    '<p>Age: ' +
+                    item.slider[1] +
+                    '&nbsp;&nbsp; Birthday: ' +
+                    item.bday[1] +
+                    '&nbsp;&nbsp;LifeGroup: ' +
+                    item.trained[1] +
+                    '</p></li>'));
+                 }
+            }
+    });
 
 });
