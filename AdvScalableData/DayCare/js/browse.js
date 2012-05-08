d80b7e32be71af004db203063e2e083b46a8dd76
JDNorrod <Kingjadien@aol.com>
2012-05-07 21:51:46 -0700

    XML and json working

--- AdvScalableData/DayCare/js/browse.js
+++ AdvScalableData/DayCare/js/browse.js
@@ -10,9 +10,12 @@
         'comment': ['Message: ', 'One of the coolest kids ever!']
 },
 */
+//this will load the data.json on the infants browsing page
+//load the data.json
 
 $('#infants').live('pageinit', function(){
 
+    //******************************************** load json
     $.ajax({
         url: 'xhr/data.json',
         type: 'GET',
@@ -26,19 +29,54 @@ $('#infants').live('pageinit', function(){
                     '<li class="ui-li ui-li-static ui-body-a">' +
                     '<h4>' + item.lname[1]  +
                     ', ' + item.fname[1] +
-                    '&nbsp;&nbsp;Edit &nbsp;Delete</h4>' +
+                    '</h4>' +
                     '<p>Age: ' +
                     item.slider[1] +
-                    '&nbsp;&nbsp; Birthday: ' +
-                    item.bday[1] +
+                    '&nbsp;&nbsp;' +
                     '&nbsp;&nbsp;LifeGroup: ' +
                     item.trained[1] +
                     '</p></li>');
                  }
             }
     });
+    $('#removeList').remove();
     $('#jsonList').listview('refresh');
     console.log($('#jsonList'));
+
+    //********************************************load XML
+
+    var parseXML = function (xml){
+        console.log(xml);
+        $(xml).find("item").each(function(){
+            var itemList = {};
+            itemList.fname          = $(this).find("fname").text();
+            itemList.lname          = $(this).find("lname").text();
+            itemList.slider         = $(this).find("slider").text();
+            itemList.trained        = $(this).find("trained").text();
+            console.log(itemList);
+
+            $('#twoFour').after(' ' +
+                '<li class="ui-li ui-li-static ui-body-a">' +
+                '<h4>' + itemList.lname  +
+                ', ' + itemList.fname +
+                '</h4>' +
+                '<p>Age: ' +
+                itemList.slider +
+                '&nbsp;&nbsp;' +
+                '&nbsp;&nbsp;LifeGroup: ' +
+                itemList.trained +
+                '</p></li>');
+
+        });
+
+    };
+
+    $.ajax({                                    //access the XML information
+        type: "GET",
+        url: "xhr/data.xml",
+        dataType: "xml",
+        success: parseXML
+        });
 
 
 });
