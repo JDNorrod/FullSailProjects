68fa0f12e15114134abb6e9d3a37757b9c473c3e
JDNorrod <Kingjadien@aol.com>
2012-07-19 20:02:13 -0700

    Updated to wk 3

--- AVF/SisterCareNative/SisterCareNative/www/js/camera.js
+++ AVF/SisterCareNative/SisterCareNative/www/js/camera.js
@@ -1,17 +1,34 @@
 var onCamSuccess = function(imageData) {
-        //console.log("we have success");
-        var image = document.getElementById('myImage');
-        image.src = "data:image/jpeg;base64," + imageData;
-       // image.src = Camera.DesitinationType.FILE_URI;
+    //console.log("we have success");
+    var image = $('#myImage');
+    image.attr("src", "data:image/jpeg;base64," + imageData);
+    image.height('20%');
+    image.width('30%');
+    // image.src = Camera.DesitinationType.FILE_URI;
 }
-    
+
+var onAndroidSuccess = function(imageData) {
+    var image = $('#myAndImage');
+    image.attr("src", "data:image/jpeg;base64," + imageData);
+    image.height('20%');
+    image.width('30%');
+    // image.src = Camera.DesitinationType.FILE_URI;
+}
+
 var onCamFail = function(message) {
-        alert('Failed because: ' + message);
+    alert('Failed because: ' + message);
 }
-    
-$('#camera').bind("click", function(){
-       navigator.camera.getPicture(onCamSuccess, onCamFail, { quality: 50,
-                            destinationType: Camera.DestinationType.DATA_URL,
-                                   saveToPhotoAlbum: true
-        }); 
-});
+
+$('.iosCamera').bind("click", function(){
+                     navigator.camera.getPicture(onCamSuccess, onCamFail, { quality: 50,
+                                                 destinationType: Camera.DestinationType.DATA_URL,
+                                                 saveToPhotoAlbum: true
+                                                 }); 
+                     });
+
+$('#andCamera').bind("click", function(){
+                     navigator.camera.getPicture(onAndroidSuccess, onCamFail, { quality: 50,
+                                                 destinationType: Camera.DestinationType.DATA_URL,
+                                                 saveToPhotoAlbum: true
+                                                 }); 
+                     });
